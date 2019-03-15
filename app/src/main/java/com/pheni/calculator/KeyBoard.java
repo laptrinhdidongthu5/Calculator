589afb360f12edba4bafa0522273c5a667724ed4
Mai Vĩnh Thành <34060324+maivinhthanh@users.noreply.github.com>
2019-03-15 11:03:58 +0700

    keyboard advence suscess

--- app/src/main/java/com/pheni/calculator/KeyBoard.java
+++ app/src/main/java/com/pheni/calculator/KeyBoard.java
@@ -1,12 +1,27 @@
 package com.pheni.calculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
 
 public class KeyBoard extends Activity {
+
+    public void onClickNumber(View view){
+
+        String text = view.getTag().toString();
+
+        Intent data = new Intent();
+        data.putExtra("feedback", text);
+
+        this.setResult(Activity.RESULT_OK, data);
+
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/pheni/calculator/MainActivity.java
+++ app/src/main/java/com/pheni/calculator/MainActivity.java
@@ -1,20 +1,26 @@
 package com.pheni.calculator;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.Selection;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.pheni.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     public EditText textEdit;
+
+    public static final int MY_REQUEST_CODE = 100;
+    private static final int REQUEST_CODE_KEYBOARD = 0x9345;
 
     public void onClickNumber(View view) {
         String text = view.getTag().toString();
@@ -50,6 +56,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_KEYBOARD) {
+            if(resultCode == Activity.RESULT_OK) {
+                String feedback = data.getStringExtra("feedback");
+
+                int pos = textEdit.getSelectionStart();
+                String a = textEdit.getText().toString();
+
+                textEdit.setText(a.substring(0, pos) + feedback + a.substring(pos, a.length()));
+
+                Editable etext = textEdit.getText();
+                Selection.setSelection(etext, pos + feedback.length());
+            }
+        }
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -62,7 +88,9 @@ public class MainActivity extends AppCompatActivity {
         view_open.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startActivity(new Intent(MainActivity.this, KeyBoard.class));
+                Intent intent = new Intent(MainActivity.this,KeyBoard.class);
+                startActivityForResult(intent, REQUEST_CODE_KEYBOARD);// Activity is started with requestCode 2
+
             }
         });
 
