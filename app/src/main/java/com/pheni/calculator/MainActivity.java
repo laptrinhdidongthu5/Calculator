8798cdeffe8c7ac0ae55cb15d82885137af2312c
it1006 <itk160454@gmail.com>
2019-03-16 12:47:31 +0700

    Fix UI_keyboard development UI

--- app/src/main/java/com/pheni/calculator/MainActivity.java
+++ app/src/main/java/com/pheni/calculator/MainActivity.java
@@ -1,27 +1,34 @@
 package com.pheni.calculator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.Selection;
-import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.Toast;
 
 import com.pheni.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    public EditText textEdit;
 
-    public static final int MY_REQUEST_CODE = 100;
-    private static final int REQUEST_CODE_KEYBOARD = 0x9345;
+    public EditText textEdit;                                   //Thành giải thích
 
+    public static final int MY_REQUEST_CODE = 100;                //Thành giải thích
+    private static final int REQUEST_CODE_KEYBOARD = 0x9345;    //Thành giải thích
+
+    /**
+     * Thành giải thích
+     *
+     * @param view
+     */
     public void onClickNumber(View view) {
         String text = view.getTag().toString();
 
@@ -30,12 +37,18 @@ public class MainActivity extends AppCompatActivity {
 
         textEdit.setText(a.substring(0, pos) + text + a.substring(pos, a.length()));
 
-        Editable etext = textEdit.getText();
+        Editable etext = textEdit.getText(); //cái này Thành sẽ giải thích
         Selection.setSelection(etext, pos + 1);
 //        Log.i("location", Integer.toString(a) );
 
     }
 
+
+    /**
+     * Thành giải thích
+     *
+     * @param view
+     */
     public void onClickRemove(View view) {
         if (textEdit.getText().toString().length() != 0) {
             String text = textEdit.getText().toString();
@@ -56,11 +69,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == REQUEST_CODE_KEYBOARD) {
-            if(resultCode == Activity.RESULT_OK) {
+        if (requestCode == REQUEST_CODE_KEYBOARD) {
+            if (resultCode == Activity.RESULT_OK) {
                 String feedback = data.getStringExtra("feedback");
 
                 int pos = textEdit.getSelectionStart();
@@ -82,17 +95,50 @@ public class MainActivity extends AppCompatActivity {
         final ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         textEdit = findViewById(R.id.text_result);
-        textEdit.setShowSoftInputOnFocus(false);
+        //textEdit.setShowSoftInputOnFocus(false); //không sử dụng được ở API 19: fix lại ở disableinputRetaincursor() method:
+        //hideKeyboard(this);//this ở đây chính là một MainActivity but it is not working
+        disableinputRetaincursor(textEdit); //it working success!
 
         View view_open = findViewById(R.id.btn_onKeyBoard);
-        view_open.setOnClickListener(new View.OnClickListener(){
+        view_open.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,KeyBoard.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, KeyBoard.class);
                 startActivityForResult(intent, REQUEST_CODE_KEYBOARD);// Activity is started with requestCode 2
 
             }
         });
 
+    }
+
+    /**
+     * Author: IT1006
+     * Ẩn input của activity được truyền vào
+     * Không phải ẩn một mà là tất cả các loại input
+     *
+     * @param activity
+     */
+    public static void hideKeyboard(Activity activity) {
+        View view = activity.getCurrentFocus();
+
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    /**
+     * Ẩn input của một đối tượng cụ thể
+     *
+     * @param editText
+     */
+    public static void disableinputRetaincursor(EditText editText) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+            editText.setTextIsSelectable(true);
+        } else {
+            editText.setRawInputType(InputType.TYPE_NULL);
+            editText.setFocusable(true);
+        }
     }
 }
