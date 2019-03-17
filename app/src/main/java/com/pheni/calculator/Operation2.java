4fe5d1f7326fd0adcd598573ad4649bf39e9caf1
Quyên <hoangquyenna98@gmail.com>
2019-03-17 08:22:19 +0700

    Create class Operation2
    + Xử Lý đối với 2 toán hạng.
    + Tìm Min,Max.
    + Tính Mode, Lũy thừa.

---
+++ app/src/main/java/com/pheni/calculator/Operation2.java
@@ -0,0 +1,34 @@
+package comunityuni.com;
+
+import java.lang.Math;
+
+public class Operation2 {
+	double toanHang1;
+	double toanHang2;
+
+	public Operation2(double toanHang1,double toanHang2)
+	{
+		this.toanHang1=toanHang1;
+		this.toanHang2=toanHang2;
+	}
+
+	public double TimMin()
+	{
+		return Math.min(toanHang1, toanHang2);
+	}
+
+	public double TimMax()
+	{
+		return Math.max(toanHang1, toanHang2);
+	}
+
+	public double TinhMode()
+	{
+		return toanHang1%toanHang2;
+	}
+
+	public double TinhMu()
+	{
+		return Math.pow(toanHang1, toanHang2);
+	}
+}
